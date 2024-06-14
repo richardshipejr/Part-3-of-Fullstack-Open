e115d8becaf3c9acac70943e96ec08b4e7e5b104
richardshipejr <richardshipejr@gmail.com>
2024-06-13 22:17:02 -0400

    corrected port number

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.post("/api/persons", (request, response) => {
   response.json(contacts);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
