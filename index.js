afd34840f246d7074cce411ff0126059f9ff2b9a
richardshipejr <richardshipejr@gmail.com>
2024-06-21 19:00:59 -0400

    saving data and retrieving data from database collection

--- index.js
+++ index.js
@@ -1,62 +1,66 @@
-const { response } = require("express");
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const Contact = require("./models/contact");
 const cors = require("cors");
 
-const mongoose = require("mongoose");
-const password = process.argv[2];
-console.log(process.argv);
-console.log("password", password);
+// const mongoose = require("mongoose");
+// const password = process.argv[2];
+// console.log(process.argv);
+// console.log("password", password);
 // console.log("password", process.argv);
 
-const url = `mongodb+srv://rshipejr:${password}@fullstackopenpart3.zxrdw2v.mongodb.net/phonebookContacts?retryWrites=true&w=majority&appName=FullStackOpenPart3`;
+// const url = `mongodb+srv://rshipejr:${password}@fullstackopenpart3.zxrdw2v.mongodb.net/phonebookContacts?retryWrites=true&w=majority&appName=FullStackOpenPart3`;
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+// mongoose.set("strictQuery", false);
+// mongoose.connect(url);
 
-const contactSchema = new mongoose.Schema({
-  name: String,
-  phoneNumber: String,
-});
+// const contactSchema = new mongoose.Schema({
+//   name: String,
+//   phoneNumber: String,
+// });
 
-const Contact = mongoose.model("Contact", contactSchema);
+// const Contact = mongoose.model("Contact", contactSchema);
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("dist"));
+// app.use(express.static("dist"));
 
 app.use(morgan("tiny"));
 
-let contacts = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let contacts = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
+//root
 app.get("/", (request, response) => {
-  response.send("<h1>Nestor Kimbabwe</h1>");
+  response.send("<h1>hello world</h1>");
 });
 
+//list all contacts
 app.get("/api/contacts", (request, response) => {
   Contact.find({}).then((contacts) => {
+    console.log(contacts);
     response.json(contacts);
   });
 });
@@ -65,6 +69,7 @@ const now = new Date();
 
 const time = now.toLocaleString();
 
+//number of contacts
 app.get("/info", (request, response) => {
   response.send(`<p>Phonebook has info for ${contacts.length} people</p>
   <br/>
@@ -72,19 +77,14 @@ app.get("/info", (request, response) => {
   `);
 });
 
+//look up specific contact
 app.get("/api/contacts/:id", (request, response) => {
-  const searchedId = request.params.id;
-  const searchedContact = contacts.find(
-    (contact) => contact.id.toString() === searchedId
-  );
-  if (searchedContact === undefined) {
-    return response.status(400).json({
-      error: "contact missing",
-    });
-  }
-  response.json(searchedContact);
+  Contact.findById(request.params.id).then((contact) => {
+    response.json(contact);
+  });
 });
 
+//delete a contact
 app.delete("/api/contacts/:id", (request, response) => {
   const searchedId = request.params.id;
   contacts = contacts.filter(
@@ -101,18 +101,19 @@ const generateId = () => {
 
 app.post("/api/contacts", (request, response) => {
   const body = request.body;
-  if (!body.number || !body.name) {
-    return response.status(400).json({ error: "name or number missing" });
+
+  if (body.name === undefined) {
+    return response.status(400).json({ error: "name missing" });
   }
 
-  const contact = {
-    id: generateId(),
+  const contact = new Contact({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  contacts = contacts.concat(contact);
-  response.json(contacts);
+  contact.save().then((savedContact) => {
+    response.json(savedContact);
+  });
 });
 
 app.put("/api/contacts/:id", (request, response) => {
@@ -140,7 +141,7 @@ app.put("/api/contacts/:id", (request, response) => {
   }
 });
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
